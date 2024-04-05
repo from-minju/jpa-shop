6367afb6da00bc2d099a8f0ce7af7989a81ba58a
minju <deco1202@naver.com>
2024-04-05 21:36:28 +0900

    [6-1] 컴포넌트 스캔과 의존관계 자동 주입 시작하기

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        //기존의 AppConfig를 설정정보로 사용하지 않기 위해 @Configuration붙은애는 잠시 제외시킴.
+        //@Configuration안에 @Component애노테이션이 포함되어있으므로
+)
+public class AutoAppConfig {
+}
