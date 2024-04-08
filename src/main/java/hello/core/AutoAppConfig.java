936787bab7201aee9fa882d92db0379d5c44dd49
minju <deco1202@naver.com>
2024-04-08 18:40:42 +0900

    ~[6] 필터

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         //기존의 AppConfig를 설정정보로 사용하지 않기 위해 @Configuration붙은애는 잠시 제외시킴.
         //@Configuration안에 @Component애노테이션이 포함되어있으므로
