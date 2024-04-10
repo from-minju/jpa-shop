4cdbbfc7d90912c1cb9b98d9c1ff90f7b8ace630
minju <deco1202@naver.com>
2024-04-10 21:01:53 +0900

    [7-1] 다양한 의존관계 주입 방법

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -26,4 +26,5 @@ public class OrderServiceTest {
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
+
 }
