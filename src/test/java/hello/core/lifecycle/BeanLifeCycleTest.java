8a2a8bda8da1f0caad9538abcf68345bd38237de
minju <deco1202@naver.com>
2024-04-19 17:02:31 +0900

    [8-3]빈 등록 초기화, 소멸 메서드

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,7 +18,7 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean
+        @Bean(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
@@ -27,9 +27,4 @@ public class BeanLifeCycleTest {
     }
 }
 
-//생성자 주입은 예외. 스프링빈생성단계에서 일어남.
 
-//초기화작업 이란?: 객체가 뭔가 처음 제대로 일을 시작할 때
-
-//생성자 파라미터로 넘기고...이렇게 해버리는게 낫지 않나요? -> 분리.
-//외부와 커넥션 맺고... 이런 동작행위는 별도의 초기화메소드로 분리하는게 나음.
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -30,18 +27,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
 
-//초기화,소멸 인터페이스의 단점 : 스프링 전용 인터페이스!, 스프링에 의존할수밖에없게됨. 거의사용안함. 스프링초창기때만들어진거.
